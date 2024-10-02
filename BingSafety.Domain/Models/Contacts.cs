feeb0fbc5259de07ff01e0a00dce1de82e772b0c
Christian Monrad Mortensen <ChrMonMor@gmail.com>
2024-10-02 14:28:39 +0200

    Add Domain Models. Need to think about video and sound recordings

---
+++ BingSafety.Domain/Models/Contacts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingSafety.Domain.Models {
+    public class Contacts {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ BingSafety.Domain/Models/EmergencyEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingSafety.Domain.Models {
+    public class EmergencyEvent {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime UpdatedDate {  get; set; } = DateTime.Now;
+        public string Status { get; set; } = string.Empty; 
+    }
+}
