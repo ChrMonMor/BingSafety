97d799b1162f62bcbb4b691996ee0ffdca0e561f
Christian Monrad Mortensen <ChrMonMor@gmail.com>
2024-10-04 11:25:15 +0200

    add api

---
+++ BingSafety.Api/StartupExtensions.cs
@@ -0,0 +1,35 @@
+﻿using BingSafety.Application;
+using BingSafety.Persistence;
+
+namespace BingSafety.Api {
+    public static class StartupExtensions {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder) {
+            // service registrations herer!
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistenceServices(builder.Configuration);
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors(options => 
+            options.AddPolicy(
+                "open",
+                policy => policy.WithOrigins([builder.Configuration ["ApiUrl"] ?? "https://localhost:7077",
+                   builder.Configuration["MauiUrl"] ?? "https://localhost:7088"])
+                .AllowAnyMethod()
+                .SetIsOriginAllowed(pol => true)
+                .AllowAnyHeader()
+                .AllowCredentials()
+                ));
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipline(this WebApplication app) { 
+            // middelware here!
+            app.UseCors("open");
+            app.UseHttpsRedirection();
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
